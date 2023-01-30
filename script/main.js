e0d9274711f09e0566953e67926ec632f6ee26ca
bharatiTech <bharati@techvariable.com>
2023-01-31 00:08:43 +0530

    [feat][Rolling the dice] - completed

--- script/main.js
+++ script/main.js
@@ -3,14 +3,12 @@ import RollingDice from "./rollTheDice.js";
 let numberOfDiceInput = document.getElementById("number-of-dice");
 let rollDiceButton = document.getElementById("roll");
 let diceContainer = document.getElementById("dice-container");
+let diceContainer2 = document.getElementById("dice-container2");
+diceContainer2.classList.add("dice-container-2");
 
 let numberOfDice = 0;
 
-numberOfDiceInput.addEventListener("change", (event) => {
-  numberOfDice = event.target.value;
-});
-
-rollDiceButton.addEventListener("click", () => {
+const roll = () => {
   let previous = document.getElementById("innerRoot");
 
   if (previous !== null) {
@@ -18,6 +16,20 @@ rollDiceButton.addEventListener("click", () => {
   }
 
   let object = new RollingDice(numberOfDice);
-  let diceDiv = object.roll();
-  diceContainer.appendChild(diceDiv);
+
+  // let diceDiv1 = object.roll();
+  // diceContainer.appendChild(diceDiv1);
+
+  let diceDiv2 = object.rollFlex();
+  diceContainer2.appendChild(diceDiv2);
+};
+
+numberOfDiceInput.addEventListener("keydown", (event) => {
+  numberOfDice = event.target.value;
+  console.log("key", event.key);
+  if (event.key == "Enter") {
+    roll();
+  }
 });
+
+rollDiceButton.addEventListener("click", roll);
--- script/rollTheDice.js
+++ script/rollTheDice.js
@@ -13,6 +13,7 @@ export default class RollingDice {
       8: "i",
     };
   }
+
   roll() {
     let diceRoot = document.createElement("div");
     diceRoot.classList.add("dice-container");
@@ -25,11 +26,11 @@ export default class RollingDice {
 
       randomDieValues.forEach((value) => {
         let dice = document.createElement("div");
-        dice.classList.add("dice", `dice${value}`);
+        dice.classList.add("diceGrid", `dice${value}`);
 
         for (let i = 0; i < value; i++) {
           let die = document.createElement("div");
-          die.classList.add("dot");
+          die.classList.add("dotGrid");
           die.style.gridArea = this.areaNumMap[i];
           dice.appendChild(die);
         }
@@ -38,5 +39,66 @@ export default class RollingDice {
       });
     }
     return diceRoot;
+  }
+
+  rollFlex() {
+    let diceRoot = document.createElement("div");
+    diceRoot.setAttribute("id", "innerRoot");
+    diceRoot.classList.add("diceRoot");
+
+    if (this.numberOfDice < 100 && this.numberOfDice > 0) {
+      let arr = new Uint8Array(this.numberOfDice);
+      crypto.getRandomValues(arr);
+      let randomDieValues = [...arr].map((v) => (v % 6) + 1);
+      console.log(randomDieValues);
+
+      randomDieValues.forEach((value) => {
+        let dice = document.createElement("div");
+        dice.classList.add("dice");
+        dice.classList.add(`dice_${value}`);
+
+        if (value > 3) {
+          if (value % 2) {
+            let newValue = value - 1;
+            this.alignDots(newValue, dice);
+
+            let column = document.createElement("div");
+            column.classList.add("columnMid");
+            this.createDot(column);
+            dice.appendChild(column);
+            value == 5 && dice.insertBefore(column, dice.children[1]);
+          } else {
+            this.alignDots(value, dice);
+          }
+        } else {
+          for (let i = 0; i < value; i++) {
+            let die = document.createElement("div");
+            die.classList.add("dot");
+            dice.appendChild(die);
+          }
+        }
+
+        diceRoot.appendChild(dice);
+      });
+    }
+    return diceRoot;
+  }
+
+  alignDots(value, dice) {
+    let divided = value / 2;
+    for (let i = 0; i < 2; i++) {
+      let column = document.createElement("div");
+      column.classList.add("column");
+      for (let j = 0; j < divided; j++) {
+        this.createDot(column);
+      }
+      dice.appendChild(column);
+    }
+  }
+
+  createDot(column) {
+    let die = document.createElement("div");
+    die.classList.add("dot");
+    column.appendChild(die);
   }
 }
